0b788e52e5caffec702275b0479c0f5c13fcfa57
Ashik <ashikece8@gmail.com>
2025-04-22 11:21:17 +0530

    Arthmetic operator precedance

---
+++ 03_arithmetic.php
@@ -0,0 +1,26 @@
+<!-- arthmetic:
+    + - * / % **
+
+    Increment or decrement operators:
+    ++, --;
+
+    Operator Precedance
+    ()
+    **
+    * / %
+    + -
+-->
+
+<?php
+    $x = 10;
+    $y = 5;
+    $sum = null;
+
+    $sum = $x + $y;
+    echo $x ** $y; // 10 ^ 5
+    echo "<br>";
+
+    $counter = 10;
+    $counter--;
+    echo $counter;
+?>
